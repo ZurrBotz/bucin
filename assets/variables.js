e59acdc75e46d78cb5d1b31c51dc57184d3e6cd3
ZurrBotz <159263248+ZurrBotz@users.noreply.github.com>
2024-02-08 17:19:54 +0700

    Update variables.js

--- assets/variables.js
+++ assets/variables.js
@@ -1,5 +1,5 @@
 export default {
-  nicknames: ["Intan", "Tann", "Intunnn", "Intannnn"],
+  nicknames: ["Ryujin", "Riyuu", "JinJin", "Olann"],
   greetings: {
     evening: "Good Evening",
     afternoon: "Good Afternoon",
